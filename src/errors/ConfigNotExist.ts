f75b4200df524aeb2e0918907517c688a7fb2a9a
phaalonso <26033899+phaalonso@users.noreply.github.com>
2021-08-10 21:22:00 -0300

    Implementation of configurator.set

---
+++ src/errors/ConfigNotExist.ts
@@ -0,0 +1,6 @@
+export class FormatNotExist extends Error {
+	constructor(configName: string) {
+		super(`The config ${configName} not exist in the format`);
+		this.name = 'ConfigNotExist';
+	}
+}
---
+++ src/errors/InvalidConfig.ts
@@ -0,0 +1,6 @@
+export class InvalidConfig extends Error {
+	constructor(configName: string, configType: string) {
+		super(`The config ${configName} must be of the type ${configType}`);
+		this.name = 'InvalidType';
+	}
+}
